cf0e80847d2b178c7afbab08b6c32526158370d6
Miki Wiik <miki.wiik@wunderdog.fi>
2017-09-02 23:01:06 +0300

    Refactor, simplify: define io events using chained call

--- src/api/server.js
+++ src/api/server.js
@@ -4,24 +4,24 @@ import socketio from 'socket.io';
 import messages from './messages.js';
 
 const server = http.createServer();
-const io = socketio(server);
-io.on('connection', (client) => {
-    client.on('get-messages', () =>
-        io.emit('messages', messages.get())
-    );
+const io = socketio(server)
+    .on('connection', (client) => {
+        client.on('get-messages', () =>
+            io.emit('messages', messages.get())
+        );
 
-    client.on('add-message', () =>
-        io.emit('message-added', messages.add())
-    );
+        client.on('add-message', () =>
+            io.emit('message-added', messages.add())
+        );
 
-    client.on('toggle-message', (messageId) =>
-        io.emit('message-toggled', messages.toggle(messageId))
-    );
+        client.on('toggle-message', (messageId) =>
+            io.emit('message-toggled', messages.toggle(messageId))
+        );
 
-    client.on('disconnect', () =>
-        console.log('disconnect')
-    );
-});
+        client.on('disconnect', () =>
+            console.log('disconnect')
+        );
+    });
 
 server.listen(8080, () => {
     console.log('%s listening at %s', server.name, server.url);
