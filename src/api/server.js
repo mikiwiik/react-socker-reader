2731118001658f2a695c37892d6bfc32587d3629
Miki Wiik <miki.wiik@wunderdog.fi>
2017-09-02 22:59:30 +0300

    Refactor: use expressions in one-line arrow functions for added legibility

--- src/api/server.js
+++ src/api/server.js
@@ -6,21 +6,21 @@ import messages from './messages.js';
 const server = http.createServer();
 const io = socketio(server);
 io.on('connection', (client) => {
-    client.on('get-messages', () => {
-        io.emit('messages', messages.get());
-    });
+    client.on('get-messages', () =>
+        io.emit('messages', messages.get())
+    );
 
-    client.on('add-message', () => {
-        io.emit('message-added', messages.add());
-    });
+    client.on('add-message', () =>
+        io.emit('message-added', messages.add())
+    );
 
-    client.on('toggle-message', (messageId) => {
-        io.emit('message-toggled', messages.toggle(messageId));
-    });
+    client.on('toggle-message', (messageId) =>
+        io.emit('message-toggled', messages.toggle(messageId))
+    );
 
-    client.on('disconnect', () => {
+    client.on('disconnect', () =>
         console.log('disconnect')
-    });
+    );
 });
 
 server.listen(8080, () => {
