edcc1bf53abf45995beaf879ad49de15bf5e9e29
Miki Wiik <miki.wiik@wunderdog.fi>
2017-09-02 20:43:04 +0300

    Use faker to generate random messages

--- src/api/messages.js
+++ src/api/messages.js
@@ -1,6 +1,9 @@
-export function getMessages() {
-    return [
-        {id: 1, message: 'Hello World'},
-        {id: 2, message: 'Spam und Eggs'},
-    ]
+import faker from'faker';
+
+export function getMessages(size = 10) {
+    const messages = [];
+    for (let i = 0; i < size; i++) {
+        messages.push({id: i, message: faker.company.catchPhrase()})
+    }
+    return messages;
 }
