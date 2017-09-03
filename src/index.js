78cab66bc93ddd89c55bdcf735ddc2be3ec7c9cc
Miki Wiik <miki.wiik@wunderdog.fi>
2017-09-03 14:05:48 +0300

    Move Front-end related code to a separate directory

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './front-end/index.css';
+import App from './front-end/App';
+import registerServiceWorker from './front-end/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
