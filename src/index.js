e045f4cb63277b70b3a96f3c2177fe8ae1492861
Miki Wiik <miki.wiik@wunderdog.fi>
2017-09-12 21:11:02 +0300

    Cleanup: remove unused registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './front-end/index.css';
 import App from './front-end/App';
-import registerServiceWorker from './front-end/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
